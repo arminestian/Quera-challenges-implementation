9a5fa01af2f644b6a46e4191c5dd60714a09a915
arminestian <arminservatiasl@gmail.com>
2025-11-15 23:20:16 +0330

    Added the statement of the Repetition problem

---
+++ تکرار کدکاپی/Repetition.py
@@ -0,0 +1,16 @@
+n = int(input())
+
+if n % 8 == 1 or n % 8 == 5:
+    print("c")
+elif n % 8 == 2:
+    print("o")
+elif n % 8 == 3:
+    print("d")
+elif n % 8 == 4:
+    print("e")
+elif n % 8 == 6:
+    print("u")
+elif n % 8 == 7:
+    print("p")
+elif n % 8 == 0:
+    print("6")
